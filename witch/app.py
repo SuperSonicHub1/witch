6887ba9d433accd318240004bb02c8b689ba8cbd
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-02-23 20:17:19 -0500

    Make rendering vars universal across streams and VODs to make way for Turbo rewrite.

--- witch/app.py
+++ witch/app.py
@@ -10,6 +10,7 @@ turbo = Turbo(app)
 app.config.from_object(default_settings)
 app.config.from_envvar("WITCH_SETTINGS", silent=True)
 
+
 @app.route("/favicon.ico")
 def favicon():
     return redirect(
@@ -21,9 +22,11 @@ def favicon():
 def index():
     return render_template("index.html.jinja2")
 
+
 @app.route("/api/goto")
 def goto():
-    return redirect(url_for("streamer", streamer=request.args.get("streamer", ""))) 
+    return redirect(url_for("streamer", streamer=request.args.get("streamer", "")))
+
 
 @app.route("/api/proxy/<path:url>")
 def proxy(url: str):
@@ -40,28 +43,57 @@ def proxy(url: str):
         response.headers["x-url"] = url
         return response
 
+
 @app.route("/m/")
 def multi():
     pass
 
+
 @app.route("/<streamer>/")
 def streamer(streamer: str):
     info, stream_url = graphql.get_live_streamer(streamer.lower())
-    return render_template("streamer.html.jinja2", streamer=streamer, info=info, stream_url=stream_url)
+    streamer = info.user.display_name
+    return render_template(
+        "streamer.html.jinja2",
+        streamer=streamer,
+        title=streamer,
+        poster=info.user.stream.preview_image_url,
+        broadcast_title=info.user.broadcast_settings.title,
+        info=info,
+        stream_url=stream_url,
+        mirror="https://twitch.tv/" + streamer
+    )
+
 
 @app.route("/<streamer>/videos/")
 def videos(streamer: str):
     qs: bytes = request.query_string
     # Add forms
-    info = attempt_extract(f"https://twitch.tv/{streamer}/videos/?{qs.decode('ascii')}", streamer=streamer)
+    info = attempt_extract(
+        f"https://twitch.tv/{streamer}/videos/?{qs.decode('ascii')}", streamer=streamer
+    )
     return render_template("videos.html.jinja2", streamer=streamer, info=info)
 
+@app.route("/videos/<int:id_>/")
+def vod(id_: int):
+    info = attempt_extract(f"https://twitch.tv/videos/{id_}/")
+    return render_template("vod.html.jinja2",
+        info=info,
+        poster=info.get("thumbnail"),
+        title=info.get("title"),
+        broadcast_title=info.get("title"),
+        streamer=info.get("uploader"),
+        stream_url=info.get("manifest_url"),
+        mirror=info.get("webpage_url")
+    )
 
 @app.route("/<streamer>/clips/")
 def clips(streamer: str):
     qs: bytes = request.query_string
     # Add forms
-    info = attempt_extract(f"https://twitch.tv/{streamer}/clips?{qs.decode('ascii')}", streamer=streamer)
+    info = attempt_extract(
+        f"https://twitch.tv/{streamer}/clips?{qs.decode('ascii')}", streamer=streamer
+    )
     return render_template("clips.html.jinja2", streamer=streamer, info=info)
 
 
@@ -69,12 +101,8 @@ def clips(streamer: str):
 def streamer_clip(streamer: str, id_: str):
     return redirect(url_for("clip", id_=id_))
 
-@app.route('/clips/<id_>')
+
+@app.route("/clips/<id_>")
 def clip(id_: str):
     info = attempt_extract(f"https://clips.twitch.tv/{id_}")
     return render_template("clip.html.jinja2", streamer=streamer, info=info)
-
-@app.route("/videos/<int:id_>/")
-def vod(id_: int):
-    info = attempt_extract(f"https://twitch.tv/videos/{id_}/")
-    return render_template("vod.html.jinja2", info=info)
